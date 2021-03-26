2fa8ceda7402dbaace93a9b42fabd5130c805899
Nabin Katwal <nabeenkatwal7@gmail.com>
2021-03-26 07:53:05 +0545

    Initialized project

---
+++ q1/cmake-build-debug/CMakeFiles/q1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/q1.dir/main.cpp.obj"
+  "q1.exe"
+  "q1.exe.manifest"
+  "q1.lib"
+  "q1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/q1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
