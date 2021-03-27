0469d8a2d77dd8eaa184a1c4281176957ce37f2c
Nabin Katwal <nabeenkatwal7@gmail.com>
2021-03-27 19:21:50 +0545

    Added problem 2

---
+++ q2/cmake-build-debug/CMakeFiles/q2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/q2.dir/main.cpp.obj"
+  "q2.exe"
+  "q2.exe.manifest"
+  "q2.lib"
+  "q2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/q2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
