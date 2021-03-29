0fd03f6fe0c7df2794862d3c04cdf2e15afbe1a5
Nabin Katwal <nabeenkatwal7@gmail.com>
2021-03-29 07:19:34 +0545

    Added question3

---
+++ q3/cmake-build-debug/CMakeFiles/q3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/q3.dir/main.cpp.obj"
+  "q3.exe"
+  "q3.exe.manifest"
+  "q3.lib"
+  "q3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/q3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
